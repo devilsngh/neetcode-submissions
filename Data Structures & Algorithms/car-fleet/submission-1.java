cb857a6e783af8154766c7de33b34a3001b7c255
Devinder Singh <81465333+devilsngh@users.noreply.github.com>
2026-08-27 23:40:37 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        List<int[]> list = new ArrayList<>();
+        int n = speed.length;
+        for (int i = 0; i < n; i++) {
+            list.add(new int[] {position[i], speed[i]});
+        }
+
+        Collections.sort(list, (a, b) -> Integer.compare(b[0], a[0]));
+
+        Stack<Double> stack = new Stack<>();
+        for (int[] p : list) {
+            stack.push((double) (target - p[0]) / p[1]);
+
+            if (stack.size() >= 2 && stack.get(stack.size() - 1) <= stack.get(stack.size() - 2)) {
+                stack.pop();
+            }
+        }
+
+        return stack.size();
+    }
+}
