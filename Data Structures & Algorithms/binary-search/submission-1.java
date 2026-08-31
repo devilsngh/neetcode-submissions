6be89d027bcde10680253416fee6e8b889f6eb50
Devinder Singh <81465333+devilsngh@users.noreply.github.com>
2026-08-31 22:16:46 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        return helper(nums, target, l, r);
+    }
+
+    private int helper(int[] nums, int target, int l, int r) {
+        if (l > r) {
+            return -1;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        if (nums[mid] == target) return mid;
+
+        if (nums[mid] < target) {
+            return helper(nums, target, mid + 1, r);
+        }
+
+        return helper(nums, target, l, mid - 1);
+    }
+}
